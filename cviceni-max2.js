21b135bda352efe570b62f89e941a20c5dc5c4a4
Kristyna Podana <kristyna.zam@gmail.com>
2023-11-13 12:58:06 +0100

    Novy soubor

---
+++ cviceni-max2.js
@@ -0,0 +1,21 @@
+const a = prompt("Číslo 1:")
+const b = prompt("Číslo 2:")
+
+/* const max2 = (a, b) => {
+    if (a > b) {
+        return a;
+    } else if (a > b) {
+        return b;
+    } else {
+        return false;
+    }
+}
+*/
+
+const max2 = (a, b) => {
+    if (a > b) {
+      return a;
+    } else {
+      return b;
+    }
+  };
